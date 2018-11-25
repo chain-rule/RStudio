6d68f1fb9010c160bd6c57d2de2f474674ecf603
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-11-25 11:34:31 +0100

    Install devtools and testthat

--- setup.R
+++ setup.R
@@ -3,10 +3,12 @@ packages <- c(
   'caTools',
   'car',
   'corrplot',
+  'devtools',
   'ggrepel',
   'glmnet',
   'plotly',
   'rmarkdown',
+  'testthat',
   'tidyverse'
 )
 install.packages(packages, repos = 'http://cran.us.r-project.org')
